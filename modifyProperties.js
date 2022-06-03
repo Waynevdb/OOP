66f3dc29c81cb787206d81eddd57a6f4d5b00737
Wayne Van Der Berg <102765856+Waynevdb@users.noreply.github.com>
2022-06-03 14:21:38 +0200

    Create modifyProperties.js

---
+++ modifyProperties.js
@@ -0,0 +1,18 @@
+let house = {
+  color: 'green',
+  numRooms: 4,
+  numWindows: 8,
+  forSale: false
+};
+//Write an expression to delete the numWindows property from house.
+delete house.numWindows
+
+
+//Consider the updated house object from above:
+let house = {
+  color: 'green',
+  numRooms: 4,
+  forSale: false
+};
+//Write an expression to add a new hasGarage property to house. Set the value of the hasGarage property to true.
+house.hasGarage= true
